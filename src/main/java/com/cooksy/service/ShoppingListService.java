567563aa036018eb3b238ce4b7193c7365ef2e16
ankielbowicz <an.kielbowicz@gmail.com>
2020-12-03 23:04:06 +0100

    Added methods in shoppinglistservie

--- src/main/java/com/cooksy/service/ShoppingListService.java
+++ src/main/java/com/cooksy/service/ShoppingListService.java
@@ -1,6 +1,10 @@
 package com.cooksy.service;
 
 
+import com.cooksy.dto.ProductDto;
+import com.cooksy.dto.ShoppingListDto;
+import com.cooksy.model.Product;
+import com.cooksy.model.ShoppingList;
 import com.cooksy.repository.ShoppingListRepository;
 
 import com.cooksy.util.converter.ShoppingListDtoToShoppingListConverter;
@@ -8,6 +12,9 @@ import com.cooksy.util.converter.ShoppingListToShoppingListDtoConverter;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.StreamSupport;
 
 @Service
 @Data
@@ -17,6 +24,25 @@ public class ShoppingListService {
     private final ShoppingListDtoToShoppingListConverter shoppingListDtoToShoppingListConverter;
     private final ShoppingListToShoppingListDtoConverter shoppingListToShoppingListDtoConverter;
 
+
+    public List<ShoppingListDto> getAll() {
+        return shoppingListToShoppingListDtoConverter.convertAll((List<ShoppingList>) shoppingListRepository.findAll());
+    }
+
+    public void saveShoppingList(ShoppingListDto shoppingListDto) {
+        shoppingListRepository.save(shoppingListDtoToShoppingListConverter.convert(shoppingListDto));
+    }
+
+    public ShoppingListDto getShoppingListByID(Long id) {
+        if (shoppingListRepository.findById(id).isPresent()) {
+            return shoppingListToShoppingListDtoConverter.convert(shoppingListRepository.findById(id).get());
+        }
+        return null;  //??
+    }
+
+    public void deleteShoppingList(ShoppingListDto shoppingListDto) {
+        shoppingListRepository.delete(shoppingListDtoToShoppingListConverter.convert(shoppingListDto));
+    }
 
 
 }
