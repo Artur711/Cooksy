c758184f9444160dfa306d430160c1fc97eab5da
Dariusz Raba <rabadariusz@gmail.com>
2020-12-20 11:29:49 +0100

    add fixed display products

--- src/main/java/com/cooksy/backdoor/ProductBackDoor.java
+++ src/main/java/com/cooksy/backdoor/ProductBackDoor.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin("http://localhost:4200")
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/v2/products")
