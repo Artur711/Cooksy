42cf85e2c9e7433b36aeffa7388399e2a3d56621
Flloki5 <rozli@op.pl>
2021-02-13 14:52:07 +0100

    Clean code

--- front-end/cooksy/src/app/app-routing.module.ts
+++ front-end/cooksy/src/app/app-routing.module.ts
@@ -15,20 +15,20 @@ import {AlwaysAuthGuard} from "./guards/always-auth.guard";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/cooksy', pathMatch: 'full', canActivate: [AlwaysAuthGuard]},
-  { path: 'cooksy', component: FrontPageComponent, canActivate: [AlwaysAuthGuard]},
-  { path: 'login', component: LoginComponent, canActivate: [AlwaysAuthGuard]},
-  { path: 'register', component: RegisterComponent, canActivate: [AlwaysAuthGuard]},
-  { path: 'menu', component: MenuComponent, canActivate: [AuthGuard, AlwaysAuthGuard],
+  { path: '', redirectTo: '/cooksy', pathMatch: 'full'},
+  { path: 'cooksy', component: FrontPageComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'menu', component: MenuComponent, canActivate: [AuthGuard],
   children: [
-    { path: 'home', component: HomeComponent, canActivate: [AuthGuard, AlwaysAuthGuard]},
-    { path: 'recipes', component: RecipeComponent, canActivate: [AuthGuard, AlwaysAuthGuard]},
-    { path: 'recipes/:page', component:RecipeComponent, canActivate: [AuthGuard, AlwaysAuthGuard]},
-    { path: 'recipes/detail/:id', component: RecipeDetailsComponent, canActivate: [AuthGuard, AlwaysAuthGuard]},
-    { path: 'shopping-lists', component: ShoppingListComponent, canActivate: [AuthGuard, AlwaysAuthGuard]},
-    { path: 'favorites', component: FavoritesComponent, canActivate: [AuthGuard, AlwaysAuthGuard]},
-    { path: 'favorites/detail/:id', component: RecipeDetailsComponent, canActivate: [AuthGuard, AlwaysAuthGuard]},
-    // { path: 'setting', component: HomeComponent, canActivate: [AuthGuard, AlwaysAuthGuard]}
+    { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+    { path: 'recipes', component: RecipeComponent, canActivate: [AuthGuard]},
+    { path: 'recipes/:page', component:RecipeComponent, canActivate: [AuthGuard]},
+    { path: 'recipes/detail/:id', component: RecipeDetailsComponent, canActivate: [AuthGuard]},
+    { path: 'shopping-lists', component: ShoppingListComponent, canActivate: [AuthGuard]},
+    { path: 'favorites', component: FavoritesComponent, canActivate: [AuthGuard]},
+    { path: 'favorites/detail/:id', component: RecipeDetailsComponent, canActivate: [AuthGuard]},
+    // { path: 'setting', component: HomeComponent, canActivate: [AuthGuard]}
     ]},
   { path: '**', component: PageNotFoundComponent}
 ];
