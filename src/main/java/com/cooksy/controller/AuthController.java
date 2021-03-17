b4d3b5ba61467dd69a5be5ed457d85d1f15850da
Artur <arturkubiak9@gmail.com>
2021-03-17 14:55:11 +0100

    Test2

--- src/main/java/com/cooksy/controller/AuthController.java
+++ src/main/java/com/cooksy/controller/AuthController.java
@@ -14,7 +14,6 @@ import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-@CrossOrigin(origins = {"http://localhost:4200", "https://cooksy-api.herokuapp.com/*"})
 @RestController
 @AllArgsConstructor
 @Slf4j
@@ -24,12 +23,14 @@ public class AuthController {
     private final JwtUtils jwtUtils;
 
     @PostMapping(value = "/register", consumes = APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = {"http://localhost:4200", "https://cooksy-api.herokuapp.com"})
     @ResponseStatus(CREATED)
     public void register(@RequestBody UserDto userDto)   {
         userService.register(userDto);
     }
 
     @PostMapping(value = "/login", consumes = APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = {"http://localhost:4200", "https://cooksy-api.herokuapp.com"})
     @ResponseStatus(OK)
     public JwtResponse login (@RequestBody CredentialsDto credentialsDto) {
         Authentication authentication = userService.login(credentialsDto);
